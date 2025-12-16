9b4e1b71e27ccfe7b0ce49f13d0b71c0a90f788c
Your GitHub Username <praneethyadav3956@gmail.com>
2025-12-16 17:05:46 +0530

    updated hots

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from API 🚀" });
 });
 
-app.listen(3000);
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running");
+});
