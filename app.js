05ff871fc916de81969d8d7f1bd3cb254c47ef4b
Your GitHub Username <praneethyadav3956@gmail.com>
2025-12-16 17:07:36 +0530

    updated health

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from API 🚀" });
 });
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok", message: "API is healthy 🚀" });
+});
+
 app.listen(3000, "0.0.0.0", () => {
   console.log("Server running");
 });
